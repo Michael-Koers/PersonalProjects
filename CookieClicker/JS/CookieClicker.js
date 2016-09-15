e5ec2e2df7c2b9a71907fced10ebecbd4a85d64d
Michael Koers <michaelkoers95@gmail.com>
2016-09-15 19:37:02 +0200

    CookieClicker - fuck spanś :D

--- CookieClicker/JS/CookieClicker.js
+++ CookieClicker/JS/CookieClicker.js
@@ -1,3 +1,12 @@
 /**
  * Created by developer on 15-9-16.
  */
+
+
+function clickCookie() {
+    var counter = document.getElementById("counter");
+    counter.textContent++;
+}
+
+var cookie = document.getElementById("cookie");
+cookie.addEventListener("click", clickCookie);
