2e9a33f5d41c673be3e22ebbab9be54861dc6af6
Michael Koers <michaelkoers95@gmail.com>
2016-09-17 01:53:38 +0200

    CookieClicker - Got first upgrade working:D

--- CookieClicker/JS/CookieClicker.js
+++ CookieClicker/JS/CookieClicker.js
@@ -1,15 +1,18 @@
-/**
- * Created by developer on 15-9-16.
- */
+'use strict';
 
-Game = {};
-Upgrades = {};
+var Game = {};
+var Upgrades = {};
 
-Upgrades.upgrade1 = function(){
-  this.cookiesPs = 1;
+var Upgrade1 = {
+    cookiesPerSecond: 0.1,
+    cost: 10
 };
 
-Game.init = function(){
+Upgrades.getCookiesPerSecond = function () {
+    return 0.1;
+};
+
+Game.init = function () {
     var cookie = document.getElementById("cookie");
     cookie.addEventListener("click", clickCookie);
 
@@ -17,8 +20,9 @@ Game.init = function(){
     upgrade1.addEventListener("click", clickUpgrade1);
 };
 
-Game.launch = function(){
+Game.launch = function () {
 
+    this.cookiePerClick = 1;
     this.cookies = 0;
     this.fps = 30;
     this.upgrade1 = 0;
@@ -28,36 +32,60 @@ Game.launch = function(){
     Game.loop();
 };
 
-Game.loop = function(){
-
+Game.loop = function () {
     Game.logic();
-
-    setTimeout(Game.loop, 1000/Game.fps);
+    setTimeout(Game.loop, 1000 / Game.fps);
 };
 
-Game.add = function(amount){
-    this.cookies+= amount;
+Game.add = function (amount) {
+    this.cookies += amount;
 };
 
-Game.logic = function(){
+Game.deduct = function (amount) {
+    this.cookies -= amount;
+};
 
+Game.logic = function () {
+    Game.add(Game.calculateCPS() / Game.fps);
+    Game.updateCookieCounter();
+};
 
+Game.updateCookieCounter = function () {
+    var counter = document.getElementById("cookieCounter");
+    counter.innerHTML = Math.floor(Game.cookies);
+    var cps = document.getElementById("cookiePerSecond");
+    cps.innerHTML = Math.floor(Game.upgrade1 * Upgrade1.cookiesPerSecond * 100)/100;
+};
+
+Game.calculateCPS = function () {
+    return this.upgrade1 * Upgrade1.cookiesPerSecond;
 };
 
 function clickCookie() {
-    var counter = document.getElementById("cookieCounter");
-    Game.cookies++;
-    counter.innerHTML = Game.cookies;
+    Game.add(Game.cookiePerClick);
+    Game.updateCookieCounter();
 }
 
-function clickUpgrade1(){
+function clickUpgrade1() {
     var upgrade1Counter = document.getElementById("upgrade1counter");
-    if(Game.cookies >= 10)
-    {
-        Game.cookies -= 10;
-        document.getElementById("cookieCounter").innerHTML = Game.cookies;
+    if (Game.cookies >= Upgrade1.cost) {
+        Game.cookies -= Upgrade1.cost;
         Game.upgrade1++;
         upgrade1Counter.innerHTML = Game.upgrade1;
+    }
+    else{
+        var warning = document.createElement("li");
+        var list = document.getElementById("upgradeList");
+        warning.style.color = "red";
+        warning.setAttribute("id", "upgrade1warning");
+        warning.innerHTML = "You need " + Upgrade1.cost + " cookies for this upgrade";
+        list.appendChild(warning);
+        setTimeout(function(){
+            var warning = document.getElementById("upgrade1warning");
+            var list = document.getElementById("upgradeList");
+            list.removeChild(warning);
+        }, 3000);
+
     }
 }
 
